31cde6745d89df8483470cdbb60621178e2e1ba6
Lucas Estevao <lucas_fernandes_estevao@hotmail.com>
2021-05-09 20:15:03 -0300

    Arrumando

--- src/js/router.js
+++ src/js/router.js
@@ -1,81 +1,81 @@
 const routers = new class Router {
-	constructor () {
-		$(window).on('hashchange', e => this.loadPage());
-		this.init();
-	}
+    constructor() {
+        $(window).on('hashchange', e => this.loadPage());
+        this.init();
+    }
 
-	init () {
-		this.loadPage();
-	}
+    init() {
+        this.loadPage();
+    }
 
-	allRoutes () {
-		return [
-		    {id: '', url: 'src/html/pages/home.html'},
-		    {id: 'quemSouEu', url: 'src/html/pages/quemSouEu.html'},
-		    {id: 'portifolio', url: 'src/html/pages/portifolio.html'},
-		    {id: 'contato', url: 'src/html/pages/contato.html'},
-		];
-	}
+    allRoutes() {
+        return [
+            { id: '', url: 'src/html/pages/home.html' },
+            { id: 'quemSouEu', url: 'src/html/pages/quemSouEu.html' },
+            { id: 'portifolio', url: 'src/html/pages/portifolio.html' },
+            { id: 'contato', url: 'src/html/pages/contato.html' },
+        ];
+    }
 
-	getPage (pageCode) {
-		if (window.innerWidth <= 991) {
-			$('.js-menu').click()
-		}
+    getPage(pageCode) {
+        if (window.innerWidth <= 991) {
+            $('.js-menu').click()
+        }
 
-		let page = this.allRoutes().find( e => e.id == pageCode );
+        let page = this.allRoutes().find(e => e.id == pageCode);
 
-		if (!page) {
-			window.location = '/';
-		}
+        if (!page) {
+            window.location = '/';
+        }
 
-		return page;
-	}
+        return page;
+    }
 
-	loadPage () {
+    loadPage() {
         const hashPage = location.hash.replace(/[#\/]/g, '');
-		const page = this.getPage(hashPage);
+        const page = this.getPage(hashPage);
 
-		this.addComponents($('.js-content-body'), page);
-	}
+        this.addComponents($('.js-content-body'), page);
+    }
 
-	addHeader (id) {
-        const headerConfig = {id, url:'src/html/components/header.html'};
+    addHeader(id) {
+        const headerConfig = { id, url: 'src/html/components/header.html' };
         const $header = $('.js-content-header');
 
         routers.addComponents($header, headerConfig);
     }
 
-	addComponents ($place, page) {
-		loader.init();
+    addComponents($place, page) {
+        loader.init();
 
-		setTimeout(() => {
-			$.ajax({
-				url: page.url,
-				type: 'GET',
-			})
-			.done(function(data) {
-				$place.html(data);
+        setTimeout(() => {
+            $.ajax({
+                    url: page.url,
+                    type: 'GET',
+                })
+                .done(function(data) {
+                    $place.html(data);
 
-				if ( page.id != '' ) {
-					const $btn = $('.js-start-particles');
-					const $particles = $('.particles-js-canvas-el');
+                    if (page.id != '') {
+                        const $btn = $('.js-start-particles');
+                        const $particles = $('.particles-js-canvas-el');
 
-					$(`.js-header .js-link`).removeClass('active');
-					$(`.js-header .js-link[href="#${page.id}"]`).addClass('active');
+                        $(`.js-header .js-link`).removeClass('active');
+                        $(`.js-header .js-link[href="#${page.id}"]`).addClass('active');
 
-					if ( $particles.length === 1 ) {
-						$btn.addClass('active');
-					} else {
-						$btn.removeClass('active');
-					}
-				}
-			})
-			.fail(function() {
-				console.log("error");
-			})
-			.always(function() {
-				loader.close();
-			});
-		}, 500);
-	}
+                        if ($particles.length === 1) {
+                            $btn.addClass('active');
+                        } else {
+                            $btn.removeClass('active');
+                        }
+                    }
+                })
+                .fail(function() {
+                    console.log("error");
+                })
+                .always(function() {
+                    loader.close();
+                });
+        }, 500);
+    }
 }();
